eac10f6381aafd40d901d5949a4a77bb90a87864
Dave <dave@orangetreesolutions.ca>
2020-04-22 09:08:44 -0700

    drop build artifacts from git

--- dist/lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Circle } from "./curves/Circle";
-import { Conic } from "./curves/Conic";
-import { TangentConvexTorus } from "./curves/TangentConvexTorus";
-import { TangentLine } from "./curves/TangentLine";
-import { LensCurve } from "./LensCurve";
-import { LensProfile } from "./LensProfile";
-import { SimpleCornea } from "./SimpleCornea";
-import { circularSag, conicSag, convertDTomm, convertmmToD, eccFromShape, getInputD, getInputmm, shapeFromEcc, solveCircleRadiusOnYAxis, toDegrees, toRadians } from "./utilities";
-export { circularSag, conicSag, solveCircleRadiusOnYAxis, convertDTomm, convertmmToD, getInputD, getInputmm, shapeFromEcc, eccFromShape, toRadians, toDegrees, LensCurve, LensProfile, SimpleCornea, Circle, Conic, TangentConvexTorus, TangentLine, };
